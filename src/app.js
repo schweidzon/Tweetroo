8280e42f1c78c070cdec700f4740e87cfa2ea473
Schweidzon <dannielsch@gmail.com>
2023-01-05 22:07:09 -0300

    Adicionando funções de POST

--- src/app.js
+++ src/app.js
@@ -3,20 +3,39 @@ import cors from 'cors'
 
 const app = express()
 
+app.use(express.json())
+
 app.use(cors())
 
+const users = []
+
 const tweets = [
-	{
-		username: "bobesponja",
-		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-		tweet: "eu amo o hub"
-	},
-    {
-		username: "bobesponja",
-		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-		tweet: "eu amo o hub"
-	},
+	
 ]
+
+app.post("/sign-up", (req, res) => {
+	const newUser = req.body
+	console.log(newUser)
+	users.push(newUser)
+	res.send("Ok")
+
+	
+})
+
+app.post("/tweets", (req, res) => {
+	const newTweet = req.body
+	users.forEach((user) => {
+		if(user.username === newTweet.username) {
+			
+			newTweet.avatar = user.avatar
+		}
+	})
+	console.log(newTweet)
+	tweets.push(newTweet)
+
+	res.send(tweets)
+})
+
 
 app.get("/tweets", (req,res) => {
     res.send(tweets)
